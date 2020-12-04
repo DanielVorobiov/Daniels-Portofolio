c67494edb3f76d2340e772ea7e78e2fb588ed5be
MarinaUTM <58036789+MarinaUTM@users.noreply.github.com>
2020-12-04 22:09:50 +0200

    Create filetypetest.py

---
+++ Portofolio/tests/filetypetest.py
@@ -0,0 +1,9 @@
+import os
+from django.test import TestCase
+
+class FileTypeTestCase(TestCase):   
+
+
+def test_file(self):
+        for filename in os.listdir("./media/gallery"):
+            self.assertEqual(filename.endswith(".jpg"), True)
